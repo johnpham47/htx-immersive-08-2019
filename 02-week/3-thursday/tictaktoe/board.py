45eae0c1d8f3dfcd8fa8f0403603438fb7fe0b3e
John Pham <johnpham47@gmail.com>
2019-09-06 13:10:29 -0500

    Made the testing board.

--- 02-week/3-thursday/tictaktoe/board.py
+++ 02-week/3-thursday/tictaktoe/board.py
@@ -1,7 +1,49 @@
-game_board = [
-              [0, 0, 0],
-              [0, 0, 0],
-              [0, 0, 0]
+# playing_board = [["_" for j in range(3)] for i in range(3)]
+# player = "X"
+board = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
 ]
+print(board[0])
+print(board[1])
+print(board[2])
 
-print(game_board)
+# def Board():
+#     for i in range(3):
+#         row = "| "
+#         for j in range(3):
+#             row += (board[i][j] + " | ")
+#         print(row)
+
+
+
+
+
+# def Location():
+#     valid = True/False:
+#     row, col = AskMove()
+#     while not valid:
+#         if row <= 0 and row >= 3 and col <= 0 and col >= 3:
+#             print("Invalid input. Please select a value between 1 and 3")
+#             row, col = AskMove()
+
+#         elif board[row][col] != "_":
+#             print("That spot is already taken. Please select another value between 1 and 3")
+#             row, col = AskMove()
+        
+#         else:
+#             board[row][col] = player
+            
+
+
+        
+
+      
+
+def SwitchPlayer():
+    if player == "X":
+        player == "Y"
+    elif player == "Y":
+        player == "X"
+    return player
--- 02-week/3-thursday/tictaktoe/tictaktoe.py
+++ 02-week/3-thursday/tictaktoe/tictaktoe.py
@@ -1,2 +1,6 @@
-def tictactoe(board, location, player)
+#Board is a nested 3x3 array
+#Location is a 2-item tuple that represents each cell on the board
+#Player is a string (X or Y) 
+
+def move(board, location, player)
     
