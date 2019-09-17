a78a8b0a17eb3606d7d95abc712a4701b8482a3b
John Pham <johnpham47@gmail.com>
2019-09-17 13:10:25 -0500

    Completed lvl 3 JS exercises.

---
+++ 04-week/0-monday/Exercises/jslvl3.js
@@ -0,0 +1,41 @@
+// Exercise 1
+function lvl3exercise1() {
+    var concat = "Hello" + " " + "World!";
+    console.log(concat);
+    return concat;
+}
+lvl3exercise1()
+
+// Exercise 2
+function lvl3exercise2() {
+    var concat = "hello" + 12
+    console.log(concat);
+    return concat;
+}
+lvl3exercise2()
+
+// Exercise 3
+function lvl3exercise3() {
+    var number = 12;
+    var string = "";
+    var concat = number + string;
+    console.log(concat);
+    return concat;
+}
+lvl3exercise3()
+
+// Exercise 4
+function lvl3exercise4() {
+    var string = "Hello World!";
+    console.log(string.length);
+    return string.length;
+}
+lvl3exercise4()
+
+// Exercise 5
+function lvl3exercise5() {
+    var string = "Hello" + "World";
+    console.log(string.indexOf("World"));
+    return string.indexOf("World");
+}
+lvl3exercise5()
