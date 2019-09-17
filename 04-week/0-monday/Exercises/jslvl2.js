758bb834162d5cf04c770c44b7e3a8e5cd68c7d1
John Pham <johnpham47@gmail.com>
2019-09-17 12:54:12 -0500

    Completed lvl 2 Exercises

---
+++ 04-week/0-monday/Exercises/jslvl2.js
@@ -0,0 +1,38 @@
+// Exercise 1
+function lvl2exercise1 (num1, num2) {
+    var sum = num1 + num2;
+    console.log(sum);
+    return sum;
+}
+lvl2exercise1(2, 9)
+
+// Exercise 2
+function lvl2exercise2 (num1, num2) {
+    var difference = num1 - num2;
+    console.log(difference);
+    return difference;
+}
+lvl2exercise2(10, 12)
+
+// Exercise 3
+function lvl2exercise3(num1 , num2) {
+    var product = num1 * num2;
+    console.log(product);
+    return product;
+}
+lvl2exercise3(64, 64)
+
+// Exercise 4
+function lvl2exercise4(num1, num2) {
+    var division = num1 / num2;
+    console.log(division);
+    return division;
+}
+lvl2exercise4(64, 4)
+
+function lvl2exercise5(num1) {
+    var increment = num1 +=2; 
+    console.log(increment);
+    return increment; 
+}
+lvl2exercise5(10)
