a83e9719de9cb70cff38519dafc7ceef13974823
John Pham <johnpham47@gmail.com>
2019-09-17 18:49:17 -0500

    Completed lvl 4 JS exercises.

---
+++ 04-week/0-monday/Exercises/jslvl4.js
@@ -0,0 +1,54 @@
+// Exercise 1
+function lvl4exercise1() {
+    var correct = true;
+    console.log(correct);
+    return correct;
+}
+lvl4exercise1()
+
+// Exercise 2
+function lvl4exercise2() {
+    var incorrect = false;
+    console.log(incorrect);
+    return incorrect;
+}
+lvl4exercise2()
+
+// Exercise 3
+function lvl4exercise3() {
+    var correct = true;
+    var not = !correct;
+    console.log(not);
+    return not;
+}
+lvl4exercise3()
+
+// Exercise 4
+function lvl4exercise4(bool1, bool2) {
+    var bool1 = true;
+    var bool2 = false;
+    var and = bool1 && bool2;
+    console.log(and);
+    return and;
+}
+lvl4exercise4()
+
+// Exercise 5
+function lvl4exercise5(bool1, bool2) {
+    var bool1 = true;
+    var bool2 = false;
+    var or = bool1 || bool2;
+    console.log(or)
+    return or;
+}
+lvl4exercise5()
+
+// Exercise 6
+function lvl4exercise6(bool1, bool2) {
+    var bool1 = true;
+    var bool2 = false;
+    var equals = bool1 == bool2;
+    console.log(equals);
+    return equals;
+}
+lvl4exercise6()
