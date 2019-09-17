4f36ab99409cd234fa42e7e8425000c00ef9dc0d
John Pham <johnpham47@gmail.com>
2019-09-17 12:38:48 -0500

    Website redesign first draft and Monday exercises

---
+++ 04-week/0-monday/Exercises/js101.js
@@ -0,0 +1,58 @@
+// Exercise 1
+function lvl1exercise1() {
+    var string = "This";
+    console.log(string);
+    return string;
+}
+lvl1exercise1()
+
+// Exercise 2
+function lvl1exercise2(x) {
+    var number = x;
+    console.log(number);
+    return number;
+}
+lvl1exercise2(7)
+
+// Exercise 3
+function lvl1exercise3(x) {
+    var float = x;
+    console.log(float);
+    return float;
+}
+lvl1exercise3(6.9)
+
+// Exercise 4
+function lvl1exercise4(hello) {
+    var string = "Hello World!";
+    console.log(string);
+    return string;
+}
+lvl1exercise4()
+
+// Exercise 5
+function lvl1exercise5() {
+    var stringNumber = ["Hello World", 4]
+    console.log(stringNumber);
+    return stringNumber
+}
+lvl1exercise5()
+
+// Exercise 6
+function lvl1exercise6() {
+    var object = {
+        key1: "Hello World!",
+        key2: 4
+    }
+    console.log(object);
+    return object;
+}
+lvl1exercise6()
+
+// Exercise 7
+function lvl1exercise7() {
+    var boolean = false;
+    console.log(boolean);
+    return boolean;
+}
+lvl1exercise7()
