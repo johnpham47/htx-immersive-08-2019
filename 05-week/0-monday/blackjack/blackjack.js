c7c7c018f95e96470e9c85e7dd6aaaf4c0091340
John Pham <johnpham47@gmail.com>
2019-09-24 09:14:16 -0500

    Started Blackjack game, styled the GUI, attempting to make Deal button functional.

---
+++ 05-week/0-monday/blackjack/blackjack.js
@@ -0,0 +1,17 @@
+document.getElementById("deal-button").addEventListener("click", function(){
+    // deal logic
+    var card;
+    if (card) [
+        
+    ]
+
+    
+}); 
+
+document.getElementById("hit-button").addEventListener("click", function(){
+    // hit logic
+});
+
+document.getElementById("stand-button").addEventListener("click", function(){
+    // stand logic
+});
