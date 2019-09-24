2c6378e3dedd155f93cde5c0407d150ab1d8a76d
John Pham <johnpham47@gmail.com>
2019-09-24 13:02:05 -0500

    Added functionality to deal and hit buttons.

--- 05-week/0-monday/blackjack/blackjack.js
+++ 05-week/0-monday/blackjack/blackjack.js
@@ -1,9 +1,34 @@
-document.getElementById("deal-button").addEventListener("click", deal)
+// Dealer Hand deal
+var deal = () => {
+    // Dealer Deal
+    let img = document.createElement('img');
+    let img1 = document.createElement('img')
+    img.src = "images/9_of_hearts.png";
+    img1.src = "images/queen_of_hearts2.png";
+    document.getElementById("dealer-hand").appendChild(img);
+    document.getElementById("dealer-hand").appendChild(img1);
 
-document.getElementById("hit-button").addEventListener("click", function(){
-    // hit logic
-});
+    // Player Deal
+    let imgPlayer = document.createElement('img');
+    let img1Player = document.createElement('img')
+    imgPlayer.src = "images/9_of_hearts.png";
+    img1Player.src = "images/queen_of_hearts2.png";
+    document.getElementById("player-hand").appendChild(imgPlayer);
+    document.getElementById("player-hand").appendChild(img1Player);
+}
+document.getElementById("deal-button").addEventListener("click", deal);
 
-document.getElementById("stand-button").addEventListener("click", function(){
-    // stand logic
-});
+
+// Hit Logic
+var hit = () => {
+    let
+}
+
+
+// document.getElementById("hit-button").addEventListener("click", function(){
+//     // hit logic
+// });
+
+// document.getElementById("stand-button").addEventListener("click", function(){
+//     // stand logic
+// });
