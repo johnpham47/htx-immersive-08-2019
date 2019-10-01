ec0aa4d7a492b1572d8b979014b229082a91857d
John Pham <johnpham47@gmail.com>
2019-10-01 13:49:06 -0500

    Pulled movie data for project

--- 05-week/2-wednesday/lectures/omdb.js
+++ 05-week/2-wednesday/lectures/omdb.js
@@ -1,37 +1,31 @@
+$(document).ready(function () {
+    $('#search-term').submit(function (event) {
+        event.preventDefault();
+        var movieName = $('#query').val();
+        var movieURL = "https://www.omdbapi.com/?apikey=dcf79638&t=" + movieName;
+        var movieOptions = {
+            s: "",
+        };
 
-$.ajax({
-    type: "GET",
-    url: "http://img.omdbapi.com/?apikey=dcf79638&t=Inception;",
-    dataType: "JSON",
-    success: function(response) {
-        console.log(response);
-        showMovieDetails(response.Title, response.Released);
-    },
-    error: function(error) {
-        console.log("Error: " + error);
-    }
-})
-
-
-$.ajax({
-    type: "GET",
-    url: "http://www.omdbapi.com/?apikey=dcf79638&t=Inception;",
-    dataType: "JSON",
-    success: function(response) {
-        console.log(response);
-        showMovieDetails(response.Title, response.Released);
-    },
-    error: function(error) {
-        console.log("Error: " + error);
-    }
-})
+        function displayMovies(data) {
+            console.log(data);
+            console.log(data.Title);
+            console.log(data.Genre);
+            console.log(data.Runtime);
+            console.log(data.Plot);
+            var movieHTML = '<ul>';
+            movieHTML += '<li>' + data.Title + "</li>";
+            movieHTML += '<li>' + data.Genre + "</li>";
+            movieHTML += '<li>' + data.Runtime + "</li>";
+            movieHTML += '<li>' + data.Plot + "</li>";
+            movieHTML += '</ul>'
 
 
-function showMoviePoster()
+            $('#search-results').html(movieHTML);
+        }
+    
+    $.getJSON(movieURL, movieOptions, displayMovies);
+    
+    });
+});
 
-function showMovieDetails(title, released) {
-    var heading = $("<h2/>").text(title);
-    $("body").append(heading);
-    var released = $("<h3/>").text(released);
-    $("body").append(released);
-}
