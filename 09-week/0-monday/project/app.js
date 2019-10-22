0dfffdf35a0b7f8ad02d0fe765e9f8044cbe021f
johnpham47 <johnpham47@gmail.com>
2019-10-22 11:19:36 -0500

    Made registration and router pages

--- 09-week/0-monday/project/app.js
+++ 09-week/0-monday/project/app.js
@@ -8,14 +8,31 @@ const accountRouter = require("./routes/account");
 app.use(express.static("public"));
 app.set("view engine", "pug");
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(
+    session({
+        secret: "La li lu le lo",
+        resave: false,
+        saveUninitialized: true
+    })
+);
 app.use("/account", accountRouter)
 
 app.get('/', function (req, res) {
     res.render("index");
 })
 
-app.get("/account/login", function (req, res) {
+app.get('/registration', function (req, res) {
+    res.render('registration');
+})
+
+// app.post('registerUser'), async
+
+app.get("/login", function (req, res) {
     res.render("login");
+})
+
+app.post("/account/login", function (req, res) {
+    res.redirect("dashboard");
 })
 
 app.listen(3000);
--- 09-week/0-monday/project/routes/account.js
+++ 09-week/0-monday/project/routes/account.js
@@ -1,8 +1,44 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/account/login", function (req, res) {
-    res.render("login");
+function loginRedirect (req, res, next) {
+    if (req.session.name) {
+        res.redirect('/account/dashboard');
+    }
+    else {
+        next();
+    }
+};
+
+function authenticate (req, res, next) {
+    if (!req.session.name) {
+        res.redirect('/login');
+    }
+    else {
+        next();
+    }
+};
+
+
+router.post("/login", function (req, res) {
+    if (req.session) {
+        req.session.name = req.body.email;
+        res.redirect('dashboard');
+    }
+});
+
+router.get("/", authenticate, (req, res) => {
+    console.log(req.session)
+    res.render("account");
+});
+
+router.get("/dashboard", authenticate, (req, res) => {
+    res.render('account/dashboard');
+});
+
+router.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/login');
 });
 
 module.exports = router;
