36a500ab25f8518d4a9a565c355fcfd11e37e57c
johnpham47 <johnpham47@gmail.com>
2019-11-04 10:59:27 -0600

    Still learning React

--- 11-week/0-monday/exercise/hello-world/src/App.js
+++ 11-week/0-monday/exercise/hello-world/src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. *Dabs*
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  name = "John";
+
+  doThing = () => {};
+
+  render() {
+    return (
+      <>
+        <header className="header">Header</header>
+        <h1 className="greeting">Hello</h1>
+        <footer className="footer">Footer</footer>
+      </>
+    );
+  }
 }
 
+// Developer doesn't call instance like the one below
+// const app = new App();
+// console.log(app);
+
 export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload. *Dabs*
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
